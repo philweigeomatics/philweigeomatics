73f189561c04bf923905ab69ec9d3a1a1d4142bc
Phil Wei <philweigeomatics@hotmail.com>
2012-07-12 00:05:36 -0600

    text for my hobbies area is completed.

--- philweigeomatics/WebContent/resources/js/site_behaviours.js
+++ philweigeomatics/WebContent/resources/js/site_behaviours.js
@@ -14,17 +14,23 @@ var siteInit = function(){
 	hobbies.bikepix_relPath = "resources/img/gsxr_w_helmet_small_600by384.jpg";
 	hobbies.bookspix_relPath = "resources/img/books600by384.jpg";
 	hobbies.moviespix_relPath = "resources/img/movies_600by384.jpg";
+	hobbies.textArea_jq = $("#myhobbies_text_wrapper div");
+	
+	
 	
 	hobbies.thumbnail1_jq.click( function(){
 		hobbies.pix_jq.css("background","url(\""+ hobbies.bikepix_relPath+"\") no-repeat scroll 0 center transparent");
+		hobbies.textArea_jq.html( en_ca.myhobbies.motorcycling );
 	});
 	
 	hobbies.thumbnail2_jq.click( function(){
 		hobbies.pix_jq.css("background","url(\""+ hobbies.bookspix_relPath+"\") no-repeat scroll 0 center transparent");
+		hobbies.textArea_jq.html( en_ca.myhobbies.reading );
 	});
 	
 	hobbies.thumbnail3_jq.click( function(){
 		hobbies.pix_jq.css("background","url(\""+ hobbies.moviespix_relPath+"\") no-repeat scroll 0 center transparent");
+		hobbies.textArea_jq.html( en_ca.myhobbies.watching_movies);
 	});
 	
 };
--- philweigeomatics/WebContent/resources/js/strings.js
+++ philweigeomatics/WebContent/resources/js/strings.js
@@ -7,9 +7,13 @@ if( !en_ca ){
 }
 
 en_ca.myhobbies = {
-		motorcycling: "I have always",
-		reading:" I love reading",
-		watching_movies:"Who doesn't like watching movies?"
+		motorcycling: "<p>Ever since I was a child&#44; my dad has been pointing at cars on the street and telling me what they are. He is such a faithful car lover&#44; so he made me just like him; but there remains one big difference between him and I. I simply just love everything that has wheels.</p>"+
+		"<p>When I was 13&#44; I started to fall in love with Bike Trials. At the time&#44; I was one of the few people in my hometown adopting this fantasitc sport and I was the youngest. Two years later&#44; I became the team leader in our bicycle club and managed most of the team activities including performance shows&#44; advertisement and so on. After moving to Beijing for high school in 2005&#44; there had been very little time to keep myself on the saddle.</p>"+
+		"<p>Moving to Canada seems to help a lot with my hobbies. I have self-modified a 2004 RX8 which I sold in 2009 and now the picture shown is my new toy bought in 2010.</p>",
+		reading:"<p>I love reading. There are lots reasons why reading is good. It gives you knowledge&#44; helps you with killing your spare time&#44; and implicitly changes the way you look at a lot of things.</p>"+
+		"<p>It is true that with today&#39;s highly adopted web technologies&#44; people can find anything they want from the internet&#44; but I still like carrying a book&#44; a kindle or an iPad to read. I especially enjoying reading on public transits so I don&#39;t waste a second.</p>"+
+		"<p>I do have lots of programming books and in fact&#44; I learnt most of my programming skills and technology concepts from books.</p>",
+		watching_movies:"<p>Who doesn&#39;t like watching movies?</p>" + "<p>However&#44; watching movies isn&#39;t just about the movies that we watch. It is also about the mood and situation. A lot of times&#44; I would watch a movie myself; especially on a sunny Saturday&#44; when most people don&#39;t go watching movies&#44; I&#39;d like to sit at the far end and enjoy the empty theatre.</p>"
 };
 
 en_ca.myworks = {
